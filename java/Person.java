657b36b58beb3d618845e4d44329fe3a7fdda612
Jackson55553 <79320339+Jackson55553@users.noreply.github.com>
2021-03-06 20:46:01 +0300

    Add files via upload
    Все выполнено.

---
+++ java/Person.java
@@ -0,0 +1,17 @@
+class Person {
+    String name;
+    String position;
+    String email;
+    int phoneNumber;
+    int wage;
+    int age;
+
+Person(String name, String position, String email, int phoneNumber, int wage, int age){
+    this.name = name;
+    this.position = position;
+    this.email = email;
+    this.phoneNumber = phoneNumber;
+    this.wage = wage;
+    this.age = age;
+}
+}
