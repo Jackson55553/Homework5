26cf5f4278c1e31cadad392fdfd12d318121d34d
Jackson55553 <79320339+Jackson55553@users.noreply.github.com>
2021-03-10 14:41:56 +0300

    Update Person.java

--- java/Person.java
+++ java/Person.java
@@ -1,17 +1,33 @@
 class Person {
-    String name;
-    String position;
-    String email;
-    int phoneNumber;
-    int wage;
-    int age;
+      private String name;
+    private String position;
+    private String email;
+    private int phoneNumber;
+    private int wage;
+    private int age;
 
-Person(String name, String position, String email, int phoneNumber, int wage, int age){
-    this.name = name;
-    this.position = position;
-    this.email = email;
-    this.phoneNumber = phoneNumber;
-    this.wage = wage;
-    this.age = age;
-}
+    public int getAge() {
+        return age;
+    }
+
+    Person(String name, String position, String email, int phoneNumber, int wage, int age) {
+        this.name = name;
+        this.position = position;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.wage = wage;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", position='" + position + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNumber=" + phoneNumber +
+                ", wage=" + wage +
+                ", age=" + age +
+                '}';
+    }
 }
